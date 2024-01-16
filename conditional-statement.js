eb2ee84ed62af34237060b2dcda2d149ca1aa71c
Wakaba Kelvin <kimanik918@gmail.com>
2024-01-16 12:52:24 +0300

    swith statements

---
+++ conditional-statement.js
@@ -0,0 +1,32 @@
+let x7 = 10;
+let y7 = 20;
+
+if (x7 < y7) {
+  console.log("x7 is greater than y7");
+}
+if (x7 > y7) {
+  console.log("x7 is greater than y7");
+} else {
+  console.log("x7 is not greater than y7");
+}
+let day = 2;
+switch (day) {
+  case 0:
+    console.log("its a sunday");
+    break;
+  case 1:
+    console.log("its a monday");
+    break;
+  case 2:
+    console.log("its a tuesday");
+    break;
+  case 3:
+    console.log("its a wednesday");
+    break;
+  case 4:
+    console.log("its a thursday");
+    break;
+  default:
+    console.log("theres no such a day");
+    break;
+}
