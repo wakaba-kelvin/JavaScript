955b19d8bfb144fc8b2f2a454a328bac52bfa5b1
Wakaba Kelvin <kimanik918@gmail.com>
2024-01-17 10:05:04 +0300

    json

---
+++ index.js
@@ -0,0 +1,7 @@
+let date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getTime());
+console.log(date.toDateString());
